6091f95ab93cd9ad0335615b96165ee54f2cc8b0
hunghuynh <huynhphihung1990@gmail.com>
2013-01-13 23:22:44 +0700

    run yeoman to create twitter bootrap

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var hunghuynh.github.comApp = angular.module('hunghuynh.github.comApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+hunghuynh.github.comApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
